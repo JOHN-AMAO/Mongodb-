06b8b34d3dfaac813da75d0ab0cbf53f50e42eaa
JOHN-AMAO <amaojohn767@gmail.com>
2023-12-09 13:48:13 +0100

    error fixed

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -17,11 +18,15 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const MONGO_URL =
-  "mongodb+srv://amaojohn:RjT7GUEAnwJkIHmw@restapi.gghwjet.mongodb.net/?retryWrites=true&w=majority";
-
 const server = http.createServer(app);
 
 server.listen(8080, () => {
   console.log("server running on http://localhost:8080/");
 });
+const MONGO_URL =
+  "mongodb+srv://amaojohn:RjT7GUEAnwJkIHmw@restapi.gghwjet.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on("error", (error: Error) => console.log(error));
